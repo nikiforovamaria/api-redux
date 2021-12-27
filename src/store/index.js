f483f5dbb8a4f05b3e834cf9f058664cfd94aa91
Maria Nikiforova <nikiforovamary@gmail.com>
2021-12-28 00:50:48 +0600

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import {createStore, combineReducers} from 'redux';
+import serviceListReducer from '../reducers/serviceListReducer';
+import serviceAddReducer from '../reducers/serviceAddReducer';
+import serviceChangeReducer from '../reducers/serviceChangeReducer';
+import serviceIsLoadingReducer from '../reducers/serviseIsLoadingReducer.js';
+
+const reducer = combineReducers({
+  serviceList: serviceListReducer,
+  serviceAdd: serviceAddReducer,
+  serviceChange: serviceChangeReducer,
+  serviceIsLoadng: serviceIsLoadingReducer,
+});
+
+const store = createStore(reducer);
+
+export default store;
